69eeff3d2d174a3f949161b9d55e9855a4d19323
StevenSDove <steven.dove@gmail.com>
2022-01-17 09:14:26 -0500

    there was nothing wrong with logger except wehre it is getting the stack from

--- src/app/shared/logging.service.ts
+++ src/app/shared/logging.service.ts
@@ -4,39 +4,36 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LoggingService {
-  private _log: Function;
-  constructor() {
-    this._log = (level: LogLevel, ...params: any[]): void => {
-      const { debug, info, warn, error } = console;
-      let logger: Function = info;
-      switch (level) {
-        case LogLevel.debug:
-          logger = debug;
-          break;
-        case LogLevel.info:
-          logger = info;
-          break;
-        case LogLevel.warning:
-          logger = warn;
-          break;
-        case LogLevel.error:
-          logger = error;
-          break;
-      }
-      logger(...params);
-    };
-  }
+  log = (level: LogLevel, ...params: any[]): void => {
+    const { log, debug, info, warn, error } = console;
+    let logger: Function = info;
+    switch (level) {
+      case LogLevel.debug:
+        logger = debug;
+        break;
+      case LogLevel.info:
+        logger = info;
+        break;
+      case LogLevel.warning:
+        logger = warn;
+        break;
+      case LogLevel.error:
+        logger = error;
+        break;
+    }
+    logger(...params);
+  };
   debug(...params: any[]): void {
-    this._log(LogLevel.debug, ...params);
+    this.log(LogLevel.debug, ...params);
   }
   info(...params: any[]): void {
-    this._log(LogLevel.info, ...params);
+    this.log(LogLevel.info, ...params);
   }
   warning(...params: any[]): void {
-    this._log(LogLevel.warning, ...params);
+    this.log(LogLevel.warning, ...params);
   }
   error(...params: any[]): void {
-    this._log(LogLevel.error, ...params);
+    this.log(LogLevel.error, ...params);
   }
 }
 
