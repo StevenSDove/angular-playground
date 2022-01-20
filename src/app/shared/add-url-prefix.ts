ff96025315e939bccda2267ae53a0040b2fc69bf
StevenSDove <steven.dove@gmail.com>
2022-01-20 13:00:44 -0500

    1 file modified

--- src/app/shared/add-url-prefix.ts
+++ src/app/shared/add-url-prefix.ts
@@ -5,7 +5,7 @@ import { Injectable, Pipe, PipeTransform } from '@angular/core';
 })
 export class AddUrlPrefixPipe implements PipeTransform {
   urlPrefix: string =
-    'https://stackblitz.com/files/sdove-angular-playground/github/StevenSDove/angular-playground/master/src/';
+    'https://stackblitz.com/files/sdove-ps-angular-getting-started/github/StevenSDove/ps-angular-gettings-started/master/src/';
 
   transform(value: string): string {
     return this.urlPrefix + value;
